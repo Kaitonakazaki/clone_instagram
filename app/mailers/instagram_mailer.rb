12862dab584e8ebc1ae5a4c119e0899d43f652dd
kaito nakazaki <kaitonakazaki@gmail.com>
2018-09-22 17:55:02 +0900

    add title

--- app/mailers/instagram_mailer.rb
+++ app/mailers/instagram_mailer.rb
@@ -2,6 +2,6 @@ class InstagramMailer < ApplicationMailer
   def instagram_mail(instagram)
      @instagram = instagram
      @user_email = @instagram.user.email
-     mail to: @user_email, subject: "お問い合わせの確認メール"
+     mail to: @user_email, subject: "投稿の確認メール"
   end
 end

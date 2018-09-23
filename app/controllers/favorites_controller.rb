9204a0417d1cfae5a6356b9156761e6b7da8d380
kaito nakazaki <kaitonakazaki@gmail.com>
2018-09-23 19:29:29 +0900

    create favorites.html.erb

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,9 @@
 class FavoritesController < ApplicationController
+
+  def index
+    @favorites_instagrams = current_user.favorite_instagrams
+  end
+
   def create
     favorite = current_user.favorites.create(instagram_id: params[:instagram_id])
     redirect_to instagrams_url
